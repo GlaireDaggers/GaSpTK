c1ab0e314f7bbd50a93f7bec4cceb39ad844de69
GlaireDaggers <glairedaggers@gmail.com>
2022-11-03 13:20:52 -0700

    Initial commit

---
+++ GaSpTK.Editor/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace GaSpTK.Editor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
